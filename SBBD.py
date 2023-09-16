4369b680d08fff8ddeb7de878800af6d3ed5df44
bipul159 <115370450+bipul159@users.noreply.github.com>
2023-09-16 16:17:53 +0600

    Add files via upload

---
+++ SBBD.py
@@ -0,0 +1,57 @@
+#WRITTEN BY MR.BIPUL
+#---------import--------#
+import os
+from os import system as clr
+#---------color---------#
+RED = '\033[1;91m' #
+WHITE = '\033[1;97m' #
+GREEN = '\033[1;32m' #
+YELLOW = '\033[1;33m' #
+BLUE = '\033[1;34m' #
+ORANGE = '\033[1;35m' #
+P = '\x1b[1;97m' # 
+M = '\x1b[1;91m' # 
+H = '\x1b[1;92m' # 
+K = '\x1b[1;92m' # 
+B = '\x1b[1;94m' # 
+U = '\x1b[1;95m' # 
+O = '\x1b[1;96m' #
+N = '\x1b[0m' #
+#---------logo----------#
+logo=(f"(B)
+----------wifi+data-----------
+Owner    : Bipul Khan
+Github    : bipul159
+FB           : Bipul Khan
+Tools     : SB BD Random
+Status    : Trial 
+#-------linex def------#
+def linex():
+	     print(f'(warna------------------------------B)
+#-------clear def-------#
+def clear():
+	clr('clear)
+	print(logo)
+#---------main def-----#
+def Mr.Bipul()
+      clear()
+      print(f'{B} [{warna}01{B}] RONDOM CLONING
+      print(f'{B} [{warna}01{B}]  EXIT TERMINAL
+      option=input(f' {B}[{warna}??{B}] CHOICE MENU >> ')
+      if option in ['01','1']:
+      BD_CLONING()
+      else:
+      	exit(' THANKS FOR USING DEAR :)')
+#--------bd clone def-----#
+def BD_CLONING():
+	  clear()
+	 print(' EXAMPLE SIM CODE  : 017,019,016,018')
+	 code=input(' ENTER SIM CODE >> ')
+	linex()
+	print(' EXAMPLE LIMIT : [1000] [3000] [5000] [10000]')
+	try:
+		limit=input(' ENTER LIMIT >> ')
+		except valueError:
+			limit=50000
+			clear()
+#--------end---------#
